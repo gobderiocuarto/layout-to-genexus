50f25240889fbd371c1b5fe1e8e8f7cd513a7dd9
germansayago <german.sayago@gmail.com>
2022-03-16 13:23:18 -0300

    first commit

---
+++ js/modernizr.js
@@ -0,0 +1,86 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+	<base href="http://administraciontributaria.riocuarto.gov.ar/">
+
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">	
+<meta name="viewport" content="width=device-width, initial-scale=1"> 
+<link rel="apple-touch-icon" sizes="57x57" href="http://administraciontributaria.riocuarto.gov.ar/assets/images/favicon/apple-icon-57x57.png">
+<link rel="apple-touch-icon" sizes="60x60" href="http://administraciontributaria.riocuarto.gov.ar/assets/images/favicon/apple-icon-60x60.png">
+<link rel="apple-touch-icon" sizes="72x72" href="http://administraciontributaria.riocuarto.gov.ar/assets/images/favicon/apple-icon-72x72.png">
+<link rel="apple-touch-icon" sizes="76x76" href="http://administraciontributaria.riocuarto.gov.ar/assets/images/favicon/apple-icon-76x76.png">
+<link rel="apple-touch-icon" sizes="114x114" href="http://administraciontributaria.riocuarto.gov.ar/assets/images/favicon/apple-icon-114x114.png">
+<link rel="apple-touch-icon" sizes="120x120" href="http://administraciontributaria.riocuarto.gov.ar/assets/images/favicon/apple-icon-120x120.png">
+<link rel="apple-touch-icon" sizes="144x144" href="http://administraciontributaria.riocuarto.gov.ar/assets/images/favicon/apple-icon-144x144.png">
+<link rel="apple-touch-icon" sizes="152x152" href="http://administraciontributaria.riocuarto.gov.ar/assets/images/favicon/apple-icon-152x152.png">
+<link rel="apple-touch-icon" sizes="180x180" href="http://administraciontributaria.riocuarto.gov.ar/assets/images/favicon/apple-icon-180x180.png">
+<link rel="icon" type="image/png" sizes="192x192"  href="http://administraciontributaria.riocuarto.gov.ar/assets/images/favicon/android-icon-192x192.png">
+<link rel="icon" type="image/png" sizes="32x32" href="http://administraciontributaria.riocuarto.gov.ar/assets/images/favicon/favicon-32x32.png">
+<link rel="icon" type="image/png" sizes="96x96" href="http://administraciontributaria.riocuarto.gov.ar/assets/images/favicon/favicon-96x96.png">
+<link rel="icon" type="image/png" sizes="16x16" href="http://administraciontributaria.riocuarto.gov.ar/assets/images/favicon/favicon-16x16.png">
+<link rel="manifest" href="http://administraciontributaria.riocuarto.gov.ar/assets/images/favicon/manifest.json">
+<meta name="msapplication-TileColor" content="#ffffff">
+<meta name="msapplication-TileImage" content="http://administraciontributaria.riocuarto.gov.ar/assets/images/favicon/ms-icon-144x144.png">
+<meta name="theme-color" content="#ffffff">
+
+<title>Administración Tributaria - Gobierno de Río Cuarto </title>
+<meta name="description" content="" />
+<meta name="keywords" content=""/>
+
+<meta property="og:title" content="" />
+<meta property="og:description" content="">
+<meta property="og:image" content="" />
+
+<!-- css -->
+<link rel="stylesheet" href="http://administraciontributaria.riocuarto.gov.ar/assets/libs/bootstrap/css/bootstrap.min.css">
+<link rel="stylesheet" href="http://administraciontributaria.riocuarto.gov.ar/assets/libs/font-awesome/css/font-awesome.min.css">
+<link rel="stylesheet" href="http://administraciontributaria.riocuarto.gov.ar/assets/libs/lightbox/ekko-lightbox.min.css">
+<link rel="stylesheet" href="http://administraciontributaria.riocuarto.gov.ar/assets/libs/owl.carousel-2.2.1/css/owl.carousel.min.css">
+<!--<link rel="stylesheet" href="http://administraciontributaria.riocuarto.gov.ar/assets/libs/owl.carousel-2.2.1/css/owl.theme.default.css">-->
+<link rel="stylesheet" href="http://administraciontributaria.riocuarto.gov.ar/assets/css/main.css">
+<link href="http://administraciontributaria.riocuarto.gov.ar/assets/libs/social-sharing/social-buttons.css" rel="stylesheet">
+<!-- js -->
+<script src="http://administraciontributaria.riocuarto.gov.ar/assets/js/modernizr.js"></script>	</head>
+<body>
+	<!-- preload -->
+<div class="preload">
+	<div class="status">
+		<i class="fa fa-spinner fa-pulse fa-3x fa-fw"></i>
+	<span class="sr-only">Loading...</span>
+	</div>
+</div>
+<header>
+	<div class="container">
+		<div class="row">
+			<div class="brand_1 col-xs-8 col-sm-4 col-md-3">
+				<a href="">
+			        <img class="img-responsive" src="http://administraciontributaria.riocuarto.gov.ar/assets/images/brand/administracion-tributaria-isologo.svg" alt="Isologo Administración Tributaria">
+			    </a>
+			</div>
+			<div class="brand_2 hidden-xs col-sm-offset-4 col-sm-4 col-md-offset-6 col-md-3">
+				<a href="">
+					<img class="img-responsive" src="http://administraciontributaria.riocuarto.gov.ar/assets/images/brand/isologotipo-gestion-llamosas.svg" alt="Isologo Gobierno de Río Cuarto">
+				</a>
+			</div>
+		</div>
+	</div>
+	<div class="top-search">
+		<div class="container">
+			<div class="row">
+				<div class="col-xs-offset-1 col-xs-10 col-sm-offset-6 col-sm-6 col-md-offset-8 col-md-4">
+					<form id="buscador_nav_sm" class="pull-right" action="buscador">
+				        <div class="input-group">
+				          <input class="form-control" type="search" placeholder="Ingrese su consulta...">
+				          <span class="input-group-btn">
+				              <button id="button_search" type="submit" class="btn btn_search">
+				                <i style="color: white" class="fa fa-fw fa-search" aria-hidden="true"></i>
+				              </button>
+				          </span>
+				        </div>
+			      	</form>
+			    </div>
+	      	</div>
+	    </div>
+	</div>
+</header><meta http-equiv='refresh' content='0;URL=http://administraciontributaria.riocuarto.gov.ar/error'>
